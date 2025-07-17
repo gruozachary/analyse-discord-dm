0716be0584a69f279b3a60a3126f0a6bde44208a
Thomas Zachary <thomaszachary06@proton.me>
2025-07-17 14:16:21 +0100

    feat(all): Add puppeteer and connect to browser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+import puppeteer from "puppeteer-core";
+
+import readline from "node:readline";
+
+
+async function main() {
+    const browser = await puppeteer.connect({ browserURL: "http://127.0.0.1:9222" });
+
+    const page = await browser.newPage();
+
+    await page.goto("https://discord.com/channels/@me");
+
+    const rl = readline.createInterface(process.stdin, process.stdout);
+
+    rl.question("Navigate to your DM, then press enter...", () => rl.close());
+
+    await browser.disconnect();
+}
+
+(async () => main())()
