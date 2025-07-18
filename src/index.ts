317308f487eb7aa6bc1418d9bf25dc65a560e186
Thomas Zachary <thomaszachary06@proton.me>
2025-07-18 13:48:40 +0100

    refactor(all): Refactor message fetching into class

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,72 @@
-import puppeteer, { Page, ConsoleMessage } from "puppeteer-core";
+import puppeteer, { Page, ConsoleMessage, ElementHandle } from "puppeteer-core";
 
 import readline from "node:readline/promises";
 
-const listSelector = "#app-mount > div.appAsidePanelWrapper_a3002d > div.notAppAsidePanel_a3002d > div.app_a3002d > div > div.layers__960e4.layers__160d8 > div > div > div > div.content_c48ade > div.page_c48ade > div > div > div.content_f75fb0 > main > div.messagesWrapper__36d07.group-spacing-16 > div > div > ol";
+class Fetcher {
+    private static chatHtmlIdRegex = new RegExp(/chat-messages-\d+-(\d+)/);
+    private static messagesLoadedRegex = new RegExp(/Fetched 50 messages/);
 
-const chatHtmlIdRegex = new RegExp(/chat-messages-\d+-(\d+)/);
-const messagesLoadedRegex = new RegExp(/Fetched 50 messages/);
+    private readonly messages = new Map<string, string>;
 
-async function waitForMessageLoad(page: Page): Promise<void> {
-    return new Promise((resolve, _) => {
-        const handler = (msg: ConsoleMessage) => {
-            const text = msg.text();
-            if (text.match(messagesLoadedRegex)?.length === 1) {
-                page.off("console", handler);
-                resolve();
+    constructor(private readonly page: Page) { }
+
+    private async fetchMessages(list: ElementHandle): Promise<void> {
+        const items = (await list.$$("li"))!;
+
+        for (const item of items) {
+            const id = await item.evaluate(el => el.id);
+
+            const arr = id.match(Fetcher.chatHtmlIdRegex);
+
+            if (arr === null || arr.length < 1) {
+                throw new Error("Error parsing message ID");
             }
+
+            this.messages.set(arr[1], "text");
         }
 
-        page.on("console", handler);
-    });
+        console.log("Fetching completed.");
+    }
+
+    private async waitForMessageLoad(): Promise<void> {
+        return new Promise((resolve, _) => {
+            const handler = (msg: ConsoleMessage) => {
+                const text = msg.text();
+                if (text.match(Fetcher.messagesLoadedRegex)?.length === 1) {
+                    this.page.off("console", handler);
+                    resolve();
+                }
+            }
+
+            this.page.on("console", handler);
+        });
+    }
+
+    private async loadMessages(scroller: ElementHandle) {
+        await scroller.evaluate((el) => {
+            el.scrollTop = 0;
+        });
+
+        console.log("Waiting for new messages");
+
+        await this.waitForMessageLoad();
+
+        console.log("New messages loaded completed.");
+    }
+
+    async init(): Promise<void> {
+        await this.page.goto("https://discord.com/channels/@me");
+    }
+
+    async start(): Promise<void> {
+        const body = (await this.page.$(".scroller__36d07"))!;
+        const list = (await body.$("ol"))!;
+
+        for (let i = 0; i < 5; i++) {
+            await this.fetchMessages(list);
+            await this.loadMessages(body);
+        }
+    }
 }
 
 async function main() {
@@ -26,44 +74,13 @@ async function main() {
 
     const page = await browser.newPage();
 
-    await page.goto("https://discord.com/channels/@me");
+    const fetcher = new Fetcher(page);
+    await fetcher.init();
 
     const rl = readline.createInterface(process.stdin, process.stdout);
-
     await rl.question("Navigate to your DM, then press enter...");
 
-    const body = (await page.$(".scroller__36d07"))!;
-
-    const list = (await page.$(listSelector))!;
-
-    const messages = new Map<string, string>();
-    for (let i = 0; i < 5; i++) {
-        const items = (await list.$$("li"))!;
-
-        for (const item of items) {
-            const id = await item.evaluate(el => el.id);
-
-            const arr = id.match(chatHtmlIdRegex);
-
-            if (arr === null || arr.length < 1) {
-                throw new Error("Error parsing message ID");
-            }
-
-            messages.set(arr[1], "text");
-        }
-
-        console.log(`Iteration ${i}: Fetching completed.`);
-
-        await body.evaluate((el) => {
-            el.scrollTop = 0;
-        });
-
-        console.log(`Iteration ${i}: Scrolling completed - waiting for new messages`);
-
-        await waitForMessageLoad(page);
-
-        console.log(`Iteration ${i}: New messages loaded completed.`);
-    }
+    await fetcher.start();
 
     await browser.disconnect();
     process.stdin.pause();
