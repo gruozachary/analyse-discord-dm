d721dd1e243e6e92d37ed4250e23da44a6712e5c
Thomas Zachary <thomaszachary06@proton.me>
2025-07-18 14:33:48 +0100

    feat(fetcher): Add state checking to class

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,12 +1,29 @@
 import { Page, ConsoleMessage, ElementHandle } from "puppeteer-core";
 
+enum FetcherState {
+    Uninitialised,
+    Initialised,
+    Processing,
+    Finished,
+    Invalid
+}
+
 export default class Fetcher {
     private static chatHtmlIdRegex = new RegExp(/chat-messages-\d+-(\d+)/);
     private static messagesLoadedRegex = new RegExp(/Fetched 50 messages/);
 
+    private state: FetcherState = FetcherState.Uninitialised;
+
     private readonly messages = new Map<string, string>;
 
     constructor(private readonly page: Page) { }
+
+    private assertState(state: FetcherState) {
+        if (this.state !== state) {
+            this.state = FetcherState.Invalid;
+            throw Error("Unexpected state!");
+        }
+    }
 
     private async fetchMessages(list: ElementHandle): Promise<void> {
         const items = (await list.$$("li"))!;
@@ -53,10 +70,16 @@ export default class Fetcher {
     }
 
     async init(): Promise<void> {
+        this.assertState(FetcherState.Uninitialised);
+
         await this.page.goto("https://discord.com/channels/@me");
+        this.state = FetcherState.Initialised;
     }
 
     async start(): Promise<void> {
+        this.assertState(FetcherState.Initialised);
+        this.state = FetcherState.Processing;
+
         const body = (await this.page.$(".scroller__36d07"))!;
         const list = (await body.$("ol"))!;
 
@@ -64,5 +87,13 @@ export default class Fetcher {
             await this.fetchMessages(list);
             await this.loadMessages(body);
         }
+
+        this.state = FetcherState.Finished;
+    }
+
+    getMessages(): Map<string, string> {
+        this.assertState(FetcherState.Finished);
+
+        return this.messages;
     }
 }
