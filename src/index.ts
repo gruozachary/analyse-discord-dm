1609d11bb405d63b245745b3e9c834dfd3facc80
Thomas Zachary <thomaszachary06@proton.me>
2025-07-18 12:23:28 +0100

    feat(all): Load another set of messages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import puppeteer from "puppeteer-core";
+import puppeteer, { Page, ConsoleMessage } from "puppeteer-core";
 
 import readline from "node:readline/promises";
 
 const listSelector = "#app-mount > div.appAsidePanelWrapper_a3002d > div.notAppAsidePanel_a3002d > div.app_a3002d > div > div.layers__960e4.layers__160d8 > div > div > div > div.content_c48ade > div.page_c48ade > div > div > div.content_f75fb0 > main > div.messagesWrapper__36d07.group-spacing-16 > div > div > ol";
 
 const chatHtmlIdRegex = new RegExp(/chat-messages-\d+-(\d+)/);
+const messagesLoadedRegex = new RegExp(/Fetched 50 messages/);
+
+async function waitForMessageLoad(page: Page): Promise<void> {
+    return new Promise((resolve, _) => {
+        const handler = (msg: ConsoleMessage) => {
+            const text = msg.text();
+            if (text.match(messagesLoadedRegex)?.length === 1) {
+                page.off("console", handler);
+                resolve();
+            }
+        }
+
+        page.on("console", handler);
+    });
+}
 
 async function main() {
     const browser = await puppeteer.connect({ browserURL: "http://127.0.0.1:9222" });
@@ -16,6 +31,8 @@ async function main() {
     const rl = readline.createInterface(process.stdin, process.stdout);
 
     await rl.question("Navigate to your DM, then press enter...");
+
+    const body = (await page.$(".scroller__36d07"))!;
 
     const list = (await page.$(listSelector))!;
 
@@ -32,6 +49,15 @@ async function main() {
 
         console.log(arr[1]);
     }
+
+
+    await body.evaluate((el) => {
+        el.scrollTop = 0;
+    });
+
+    console.log("Waiting for messages...");
+    await waitForMessageLoad(page);
+    console.log("Messages received!");
 
     await browser.disconnect();
     process.stdin.pause();
